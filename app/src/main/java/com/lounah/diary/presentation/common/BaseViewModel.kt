501def30b0691b4182a8504c5084944825e4ae2e
Lounah <lounahxrussia@gmail.com>
2018-10-24 10:18:20 +0300

    base components added

---
+++ app/src/main/java/com/lounah/diary/presentation/common/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package com.lounah.diary.presentation.common
+
+import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+open class BaseViewModel : ViewModel() {
+
+    val disposables = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        if (!disposables.isDisposed) {
+            disposables.clear()
+        }
+    }
+}
